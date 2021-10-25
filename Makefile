6e048a20d3949338ef0da20f8084e543e80393f0
Esmaeill <liyanboy74@gmail.com>
2021-10-25 09:06:03 +0330

    first init

---
+++ Makefile
@@ -0,0 +1,5 @@
+default:
+	gcc ech0.c -o ech0
+
+install:
+	sudo cp ech0 /usr/local/bin
---
+++ ech0.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define SIZE_OF_BUFFER 0xfffff
+int main(int argc,char* argv[])
+{
+    FILE *fp;
+    char buff[1024];
+    size_t s,i=0;;
+    int j;
+    for(j=1;j<argc;j++)
+    {
+        fp=fopen(argv[j],"r");
+        s=fread(buff,1,SIZE_OF_BUFFER,fp);
+        printf("------- START OF [%s] --------\r\n",argv[j]);
+        for(i=0;i<s;i++)putchar(buff[i]);
+        printf("\r\n------------- %s -------------\r\n",s==SIZE_OF_BUFFER?"OVF!":"EOF");
+        fclose(fp);
+    }
+    return 0;
+}
